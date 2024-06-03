356ae9cbd747fe553ed6a894b0f63e0789fa449f
Miguel Cobian <miguelcobian99@gmail.com>
2024-06-03 13:17:44 -0600

    Feat. Seeding db

---
+++ prisma/seed.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+import { categories } from "./data/categories";
+import { products } from "./data/products";
+import { error } from "console";
+
+
+const prisma = new PrismaClient();
+
+async function main() {
+    try {
+        await prisma.category.createMany({
+            data: categories,
+        });
+        await prisma.product.createMany({
+            data: products,
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (error) => {
+        console.log(error);
+        await prisma.$disconnect();
+        process.exit(1)
+    })
