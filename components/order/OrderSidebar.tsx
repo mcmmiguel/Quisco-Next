e4be4391042482d021d6c67026207aaa3c8d858c
Miguel Cobian <miguelcobian99@gmail.com>
2024-06-03 13:48:04 -0600

    Chore. Data fetching

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,4 +1,14 @@
-const OrderSidebar = () => {
+import { prisma } from "@/src/lib/prisma";
+
+async function getCategories() {
+    return await prisma.category.findMany();
+}
+
+const OrderSidebar = async () => {
+
+    const categories = await getCategories();
+    console.log(categories);
+
     return (
         <aside className="md:w-72 md:h-screen bg-white">
 
