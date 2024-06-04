68e5e4e2adfe9df5934ebb3e3288cd2a9a7d0449
Miguel Cobian <miguelcobian99@gmail.com>
2024-06-03 19:52:16 -0600

    Feat. Add server actions, validation with zod and display messages with toastify

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -4,17 +4,33 @@ import { useStore } from "@/src/store";
 import ProductDetails from "./ProductDetails";
 import { useMemo } from "react";
 import { formatCurrency } from '../../src/utils/index';
+import { createOrder } from "@/actions/create-order-action";
+import { OrderSchema } from "@/src/schema";
+import { toast } from "react-toastify";
 
 const OrderSummary = () => {
 
     const order = useStore(state => state.order);
     const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order]);
 
+    const handleCreateOrder = (formData: FormData) => {
+        const data = {
+            name: formData.get('name'),
+        }
+        const result = OrderSchema.safeParse(data);
+        if (!result.success) {
+            result.error.issues.forEach((issue) => {
+                toast.error(issue.message);
+            })
+        }
+        createOrder();
+    }
+
     return (
         <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
             <h1 className="text-4xl text-center font-black">Mi pedido</h1>
 
-            {order.length === 0 ? <p className="text-center my-10">El carrito está vacío</p> : (
+            {order.length === 0 ? <p className="text-center my-10">El pedido está vacío</p> : (
                 <div className="mt-5">
                     {order.map(item => (
                         <ProductDetails key={item.id} item={item} />
@@ -24,6 +40,23 @@ const OrderSummary = () => {
                         Total a pagar:{''}
                         <span className="font-bold">{formatCurrency(total)}</span>
                     </p>
+
+                    <form
+                        className="w-full mt-10 space-y-5"
+                        action={handleCreateOrder}
+                    >
+                        <input
+                            type="text"
+                            placeholder="Tu nombre"
+                            className="bg-white border border-gray-100 p-2 w-full"
+                            name="name"
+                        />
+                        <input
+                            type="submit"
+                            className="p-2 rounded uppercase text-white bg-black w-full text-center cursor-pointer font-bold"
+                            value="Confirmar pedido"
+                        />
+                    </form>
                 </div>
             )}
         </aside>
