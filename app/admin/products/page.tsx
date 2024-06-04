e0212947345e7baef08c3168eed46a83b4cf1d24
Miguel Cobian <miguelcobian99@gmail.com>
2024-06-04 16:17:57 -0600

    Feat. Include table category

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,10 +1,27 @@
+import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
 
-const ProductsPage = () => {
-    console.log('Desde el servidor');
+async function getProducts() {
+    const products = await prisma.product.findMany({
+        include: {
+            category: true,
+        }
+    });
+
+    return products;
+}
+
+export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>;
+
+const ProductsPage = async () => {
+    const products = await getProducts();
+
     return (
         <>
             <Heading>Administrar productos</Heading>
+
+            <ProductTable products={products} />
         </>
     )
 };
