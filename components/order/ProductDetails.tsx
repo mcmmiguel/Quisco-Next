fa52375be71a4ed82677b1721069842bac31f039
Miguel Cobian <miguelcobian99@gmail.com>
2024-06-03 18:33:59 -0600

    Feat. Increase item quantity

--- components/order/ProductDetails.tsx
+++ components/order/ProductDetails.tsx
@@ -1,12 +1,16 @@
 import { XCircleIcon, PlusIcon, MinusIcon } from '@heroicons/react/24/outline';
 import { OrderItem } from "@/src/types"
 import { formatCurrency } from '@/src/utils';
+import { useStore } from '@/src/store';
 
 type ProductDetailsProps = {
     item: OrderItem;
 }
 
 const ProductDetails = ({ item }: ProductDetailsProps) => {
+
+    const increaseQuantity = useStore(state => state.increaseQuantity)
+
     return (
         <div className="shadow space-y-1 p-4 bg-white  border-t border-gray-200 ">
             <div className="space-y-4">
@@ -37,7 +41,7 @@ const ProductDetails = ({ item }: ProductDetailsProps) => {
 
                     <button
                         type="button"
-                        onClick={() => { }}
+                        onClick={() => increaseQuantity(item.id)}
                     >
                         <PlusIcon className="h-6 w-6" />
                     </button>
