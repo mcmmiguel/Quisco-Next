79b9f812ff19715bb85d932a8fc3a14d23dce7e1
Miguel Cobian <miguelcobian99@gmail.com>
2024-06-07 19:25:09 -0600

    Fix. Add Zod import

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
 import { Order, OrderProducts, Product } from "@prisma/client";
+import { z } from "zod";
 
 export type OrderItem = Pick<Product, 'id' | 'name' | 'price'> & {
     quantity: number;
